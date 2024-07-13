bd2ecd84e922d3f1e2b16f616b86b2cca7a8893e
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-07-14 00:20:28 +0900

    繰り返し関連

--- part1_index.js
+++ part1_index.js
@@ -7,3 +7,40 @@ if (age >= 20) {
 } else {
   console.log("子供です");
 }
+
+switch (age) {
+  case 20:
+    console.log("成人おめでとうございます");
+    break;
+  case 12:
+  case 15:
+  case 18:
+    console.log("卒業おめでとうございます");
+    break;
+  default:
+    console.log(`${age}才です`);
+}
+
+for (let i = 0; i < 5; i++) {
+  console.log("こんにちは");
+}
+
+let i = 0;
+while (i < 5) {
+  console.log("こんにちは");
+  i++;
+}
+
+let product = {
+  name: "りんご",
+  price: 100,
+};
+
+for (let key in product) {
+  console.log(key);
+}
+
+let fruits = ["りんご", "みかん", "パイナップル"];
+for (let fruit of fruits) {
+  console.log(fruit);
+}
