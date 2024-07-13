4714140bf4b5f42ebbcd8fe331183c9679235dd5
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-07-13 22:22:07 +0900

    配列、オブジェクト

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
 console.log("hello worldテストです");
 let nickname = "tarou";
 console.log(nickname);
-nickname = 'sample'
+nickname = "sample";
 console.log(nickname);
-const firstName = 'jirou'
-console.log(firstName)
+const firstName = "jirou";
+console.log(firstName);
 
+let fruit = "apple";
+let age = 20;
+let circleRation = 3.14;
+let isActive = true;
+let color;
+
+let box = null;
+console.log(typeof fruit);
+
+// オブジェクト型
+let product = {
+  name: "りんご",
+  price: 200,
+};
+
+console.log(product);
+product.name = "みかん";
+console.log(product.name);
+
+product["price"] = 300;
+console.log(product["price"]);
+
+let fruits = ["りんご", "みかん"];
+console.log(fruits);
+
+fruits[0] = "バナナ";
+console.log(fruits[0]);
+
+fruits[2] = "パイナップル";
+console.log(fruits);
+
+console.log(fruits.length)
