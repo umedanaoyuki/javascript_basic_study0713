952efc9648d7cd1517391599409e770782dbbb78
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-07-13 22:59:05 +0900

    四則演算と比較

--- index.js
+++ index.js
@@ -38,3 +38,68 @@ fruits[2] = "パイナップル";
 console.log(fruits);
 
 console.log(fruits.length)
+
+const outputProductInfo = (productName, productPrice) => {
+  console.log('こんにちは')
+  console.log(`${productName}の値段は、${productPrice}円です`)
+}
+
+outputProductInfo('みかん', 100);
+outputProductInfo("りんご", 150);
+outputProductInfo("パイナップル", 200);
+
+
+const add = (a,b) => {
+  return a + b;
+}
+
+console.log(add(1, 2))
+
+let x = 10;
+let y = 3;
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.lof(x / y);
+console.log(x % y);
+console.log(x ** 2);
+console.log(++x); //11
+console.log(x); //11
+console.log(x++); // 11
+console.log(x); // 12
+console.log(--x); //11
+console.log(x); //11
+console.log(x--); //11
+console.log(x); //10
+
+x += 5;
+console.log(x); // 15
+
+x -= 5;
+console.log(x); // 10
+
+console.log(x > 5); // true
+console.log(x > 20); // false
+
+console.log(x < 20); // true
+console.log(x < 5); // false
+
+console.log(x <= 10) // true
+
+console.log(x === 10); // true
+console.log(x === 11); // false
+
+console.log(x !== 11); // true
+console.log(x !== 10); // false
+
+console.log(1 === '1'); // false
+console.log (1 == '1') // true
+
+
+
+
+
+
+
+
+
