3455d892cd6d2e5b7f47e625a50f22d2dd5bfa10
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-07-13 23:51:34 +0900

    アンパサンドと論理和

--- index.js
+++ index.js
@@ -37,23 +37,22 @@ console.log(fruits[0]);
 fruits[2] = "パイナップル";
 console.log(fruits);
 
-console.log(fruits.length)
+console.log(fruits.length);
 
 const outputProductInfo = (productName, productPrice) => {
-  console.log('こんにちは')
-  console.log(`${productName}の値段は、${productPrice}円です`)
-}
+  console.log("こんにちは");
+  console.log(`${productName}の値段は、${productPrice}円です`);
+};
 
-outputProductInfo('みかん', 100);
+outputProductInfo("みかん", 100);
 outputProductInfo("りんご", 150);
 outputProductInfo("パイナップル", 200);
 
-
-const add = (a,b) => {
+const add = (a, b) => {
   return a + b;
-}
+};
 
-console.log(add(1, 2))
+console.log(add(1, 2));
 
 let x = 10;
 let y = 3;
@@ -84,7 +83,7 @@ console.log(x > 20); // false
 console.log(x < 20); // true
 console.log(x < 5); // false
 
-console.log(x <= 10) // true
+console.log(x <= 10); // true
 
 console.log(x === 10); // true
 console.log(x === 11); // false
@@ -92,13 +91,35 @@ console.log(x === 11); // false
 console.log(x !== 11); // true
 console.log(x !== 10); // false
 
-console.log(1 === '1'); // false
-console.log (1 == '1') // true
+console.log(1 === "1"); // false
+console.log(1 == "1"); // true
 
+let userAge = 25;
+let userType = userAge > 20 ? "adult" : "child";
+console.log(userType); // adult
 
+// x = 10
+console.log(x > 5 && x < 20); // true
+console.log(x > 15 && x < 20); // false
+console.log(x > 5 && x < 9); // false
 
+console.log(x > 5 || x < 9); // true
+console.log(x > 16 || x < 20); // true
+console.log(x > 16 || x < 9); // false
 
+console.log(!(x > 16)); // true
 
+// falsy
+undefined;
+null;
+0;
+("");
+NaN
+
+console.log(true && 'a'); // a
+console.log(false && "a"); // false
+console.log(true || 1); // true
+console.log(false || 1); // 1
 
 
 
